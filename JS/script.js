ac80d3412ef5ae45fd4f2aefbe305849003def17
actuallynotDeru <crazydean2005@gmail.com>
2024-12-09 23:14:28 +0800

    Committed fully??

--- JS/script.js
+++ JS/script.js
@@ -1,10 +1,12 @@
 function sendMail() {
-    const params = {
+    var params = {
       name: document.getElementById("name").value,
       email: document.getElementById("email").value,
       subject: document.getElementById("subject").value,
-      message: document.getElementById("message").value,
+      message: document.getElementById("message").value
     }
   
-    emailjs.send("service_yoixet3","template_9hfqdii",params).then(alert("Email Sent!!"))
+    emailjs.send("service_yoixet3","template_9hfqdii",params).then(function (res){
+      alert("Success! " + res.status);
+    })
   }
